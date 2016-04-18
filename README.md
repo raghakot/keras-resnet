12d84dc15b408bb04fce744e66a406413f52f21f
Ragha <ragha@outlook.com>
2016-04-17 19:56:41 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # keras-resnet
-Residual networks implementation using Keras-1.0 functional API
+Residual networks implementation using Keras-1.0 functional API. 
+
+### The original articles
+ * [Deep Residual Learning for Image Recognition](http://arxiv.org/abs/1512.03385) (the 2015 ImageNet competition winner)
+ * [Identity Mappings in Deep Residual Networks](http://arxiv.org/abs/1603.05027)
+
+### Residual blocks
+The residual blocks are based on the new improved scheme proposed in [Identity Mappings in Deep Residual Networks](http://arxiv.org/abs/1603.05027)
+
---
+++ resnet.py
@@ -0,0 +1,146 @@
+import os
+
+from keras.models import Model
+from keras.layers import (
+    Input,
+    Activation,
+    merge,
+    Dense,
+    Flatten
+)
+from keras.layers.convolutional import (
+    Convolution2D,
+    MaxPooling2D,
+    AveragePooling2D
+)
+from keras.layers.normalization import BatchNormalization
+from keras.utils.visualize_util import plot
+
+
+# Helper to build a conv -> BN -> relu block
+def _conv_bn_relu(nb_filter, nb_row, nb_col, subsample=(1, 1)):
+    def f(input):
+        conv = Convolution2D(nb_filter=nb_filter, nb_row=nb_row, nb_col=nb_col, subsample=subsample,
+                             init="he_normal", border_mode="same")(input)
+        norm = BatchNormalization(mode=0, axis=1)(conv)
+        return Activation("relu")(norm)
+
+    return f
+
+
+# Helper to build a BN -> relu -> conv block
+# This is an improved scheme proposed in http://arxiv.org/pdf/1603.05027v2.pdf
+def _bn_relu_conv(nb_filter, nb_row, nb_col, subsample=(1, 1)):
+    def f(input):
+        norm = BatchNormalization(mode=0, axis=1)(input)
+        activation = Activation("relu")(norm)
+        return Convolution2D(nb_filter=nb_filter, nb_row=nb_row, nb_col=nb_col, subsample=subsample,
+                             init="he_normal", border_mode="same")(activation)
+
+    return f
+
+
+# Bottleneck architecture for > 34 layer resnet.
+# Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
+# Returns a final conv layer of nb_filters * 4
+def _bottleneck(nb_filters, init_subsample=(1, 1)):
+    def f(input):
+        conv_1_1 = _bn_relu_conv(nb_filters, 1, 1, subsample=init_subsample)(input)
+        conv_3_3 = _bn_relu_conv(nb_filters, 3, 3)(conv_1_1)
+        residual = _bn_relu_conv(nb_filters * 4, 1, 1)(conv_3_3)
+        return _shortcut(input, residual)
+
+    return f
+
+
+# Basic 3 X 3 convolution blocks.
+# Use for resnet with layers <= 34
+# Follows improved proposed scheme in http://arxiv.org/pdf/1603.05027v2.pdf
+def _basic_block(nb_filters, init_subsample=(1, 1)):
+    def f(input):
+        conv1 = _bn_relu_conv(nb_filters, 3, 3, subsample=init_subsample)(input)
+        residual = _bn_relu_conv(nb_filters, 3, 3)(conv1)
+        return _shortcut(input, residual)
+
+    return f
+
+
+# Adds a shortcut between input and residual block and merges them with "sum"
+def _shortcut(input, residual):
+    # Expand channels of shortcut to match residual.
+    # Stride appropriately to match residual (width, height)
+    # Should be int if network architecture is correctly configured.
+    stride_width = input._shape[2].value / residual._shape[2].value
+    stride_height = input._shape[3].value / residual._shape[3].value
+    equal_channels = residual._shape[1].value == input._shape[1].value
+
+    shortcut = input
+    # 1 X 1 conv if shape is different. Else identity.
+    if stride_width > 1 or stride_height > 1 or not equal_channels:
+        shortcut = Convolution2D(nb_filter=residual._shape[1].value, nb_row=1, nb_col=1,
+                                 subsample=(stride_width, stride_height),
+                                 init="he_normal", border_mode="valid")(input)
+
+    return merge([shortcut, residual], mode="sum")
+
+
+# Builds a residual block with repeating bottleneck blocks.
+def residual_block(block_function, nb_filters, repetations, is_first_layer=False):
+    def f(input):
+        for i in range(repetations):
+            init_subsample = (1, 1)
+            if i == 0 and not is_first_layer:
+                init_subsample = (2, 2)
+            input = block_function(nb_filters=nb_filters, init_subsample=init_subsample)(input)
+        return input
+
+    return f
+
+
+# http://arxiv.org/pdf/1512.03385v1.pdf
+# 50 Layer resnet
+def resnet():
+    input = Input(shape=(3, 224, 224))
+
+    conv1 = _conv_bn_relu(nb_filter=64, nb_row=7, nb_col=7, subsample=(2, 2))(input)
+    pool1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), border_mode="same")(conv1)
+
+    # Build residual blocks..
+    block_fn = _bottleneck
+    block1 = residual_block(block_fn, nb_filters=64, repetations=3, is_first_layer=True)(pool1)
+    block2 = residual_block(block_fn, nb_filters=128, repetations=4)(block1)
+    block3 = residual_block(block_fn, nb_filters=256, repetations=6)(block2)
+    block4 = residual_block(block_fn, nb_filters=512, repetations=3)(block3)
+
+    # Classifier block
+    pool2 = AveragePooling2D(pool_size=(7, 7), strides=(1, 1), border_mode="same")(block4)
+    flatten1 = Flatten()(pool2)
+    dense = Dense(output_dim=1000, init="he_normal", activation="softmax")(flatten1)
+
+    model = Model(input=input, output=dense)
+    return model
+
+
+def main():
+    import time
+    start = time.time()
+    model = resnet()
+    duration = time.time() - start
+    print "{} s to make model".format(duration)
+    start = time.time()
+    model.output
+    duration = time.time() - start
+    print "{} s to get output".format(duration)
+    start = time.time()
+    model.compile(loss="categorical_crossentropy", optimizer="sgd")
+    # model.compile(loss={"distance": "binary_crossentropy"}, optimizer="sgd")
+    duration = time.time() - start
+    print "{} s to get compile".format(duration)
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    model_path = os.path.join(current_dir, "resnet_50.png")
+    plot(model, to_file=model_path, show_shapes=True)
+
+
+if __name__ == '__main__':
+    main()
