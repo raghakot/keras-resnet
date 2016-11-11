7e8618279a6f1960224233c68df7eb41fa73d85a
Ragha <ragha@outlook.com>
2016-11-11 15:08:55 -0800

    Updated avg polling to use 'valid' padding.

--- resnet.py
+++ resnet.py
@@ -113,7 +113,7 @@ def resnet():
     block4 = _residual_block(block_fn, nb_filters=512, repetations=3)(block3)
 
     # Classifier block
-    pool2 = AveragePooling2D(pool_size=(7, 7), strides=(1, 1), border_mode="same")(block4)
+    pool2 = AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(block4)
     flatten1 = Flatten()(pool2)
     dense = Dense(output_dim=1000, init="he_normal", activation="softmax")(flatten1)
 
